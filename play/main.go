0cdd7e22e9046b44a65f14f5e5e68f08bc2b54a4
Sjon <sjon@x61.eu>
2016-01-02 23:59:46 +0100

    Some play to test recent addition to the gotk package

--- play/main.go
+++ play/main.go
@@ -6,6 +6,14 @@ import (
 	"github.com/gotk3/gotk3/gtk"
 )
 
+type app struct {
+	window    *gtk.Window
+	actionBar *gtk.ActionBar
+	headerBar *gtk.HeaderBar
+	infoBar   *gtk.InfoBar
+	popover   *gtk.Popover
+}
+
 func main() {
 	gtk.Init(nil)
 	builder, err := gtk.BuilderNew()
@@ -13,47 +21,75 @@ func main() {
 		panic(err)
 	}
 	builder.AddFromFile("ui.glade")
+	var app app
 
-	var window *gtk.Window
 	obj, err := builder.GetObject("window1")
 	if err != nil {
 		panic(err)
 	}
 	if w, ok := obj.(*gtk.Window); ok {
-		window = w
+		app.window = w
 	}
-	window.Connect("destroy", gtk.MainQuit)
+	app.window.Connect("destroy", gtk.MainQuit)
 
-	var infobar *gtk.InfoBar
+	obj, err = builder.GetObject("headerbar1")
+	if err != nil {
+		panic(err)
+	}
+	if h, ok := obj.(*gtk.HeaderBar); ok {
+		app.headerBar = h
+	}
+
 	obj, err = builder.GetObject("infobar1")
 	if err != nil {
 		panic(err)
 	}
 	if i, ok := obj.(*gtk.InfoBar); ok {
-		infobar = i
+		app.infoBar = i
 	}
-	infobar.Connect("response", infobar.Hide)
+	app.infoBar.Connect("response", app.infoBar.Hide)
 
-	var actionbar *gtk.Actionbar
 	obj, err = builder.GetObject("actionbar1")
 	if err != nil {
 		panic(err)
 	}
-	if i, ok := obj.(*gtk.Actionbar); ok {
-		actionbar = i
+	if i, ok := obj.(*gtk.ActionBar); ok {
+		app.actionBar = i
 	}
-	//infobar.Connect("response", infobar.Hide)
+
+	//obj, err = builder.GetObject("popover1")
+	//if err != nil {
+	//	panic(err)
+	//}
+	//if i, ok := obj.(*gtk.Popover); ok {
+	//	app.popover = i
+	//}
+	app.popover, err = gtk.PopoverNew(app.headerBar)
+	if err != nil {
+		panic(err)
+	}
 
 	go func() {
 		for i := 0; ; i++ {
 			<-time.After(2 * time.Second)
-			if i%2 == 0 {
-				infobar.Hide()
-			} else {
-				infobar.Show()
-			}
+			toggle(app.infoBar)
+			toggle(app.actionBar)
 		}
 	}()
-	window.Show()
+	app.window.Show()
 	gtk.Main()
+}
+
+type hideShower interface {
+	Hide()
+	Show()
+	IsVisible() bool
+}
+
+func toggle(h hideShower) {
+	if h.IsVisible() {
+		h.Hide()
+	} else {
+		h.Show()
+	}
 }
