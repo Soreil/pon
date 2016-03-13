58c9cf57a639aa39530212ed8b11716636698e0a
Sjon <sjon@x61.eu>
2016-03-13 02:49:33 +0100

    Seperated out ui file extraction

--- play/main.go
+++ play/main.go
@@ -1,119 +1,66 @@
 package main
 
 import (
-	"fmt"
+	"os"
 	"time"
 
 	"github.com/gotk3/gotk3/gtk"
 )
 
 type app struct {
-	window    *gtk.Window
-	actionBar *gtk.ActionBar
-	headerBar *gtk.HeaderBar
-	infoBar   *gtk.InfoBar
-	menu      *gtk.Menu
-	settings  *gtk.Settings
+	window           *gtk.Window
+	actionBar        *gtk.ActionBar
+	headerBar        *gtk.HeaderBar
+	infoBar          *gtk.InfoBar
+	menu             *gtk.Menu
+	settings         *gtk.Settings
+	isDarkTheme      *gtk.CheckMenuItem
+	isActionsEnabled *gtk.CheckMenuItem
+}
+
+func init() {
+	gtk.Init(&os.Args)
 }
 
 func main() {
-	gtk.Init(nil)
-	builder, err := gtk.BuilderNew()
+	const builderFile = "../ui/ui.glade"
+	app, err := GetObjects(builderFile)
 	if err != nil {
 		panic(err)
 	}
-	builder.AddFromFile("ui.glade")
-	var app app
 
-	settings, err := gtk.SettingsGetDefault()
-	if err != nil {
-		panic(err)
-	}
-	app.settings = settings
-
-	obj, err := builder.GetObject("window1")
-	if err != nil {
-		panic(err)
-	}
-	if w, ok := obj.(*gtk.Window); ok {
-		app.window = w
-	}
 	app.window.Connect("destroy", gtk.MainQuit)
-
-	obj, err = builder.GetObject("headerbar1")
-	if err != nil {
-		panic(err)
-	}
-	if h, ok := obj.(*gtk.HeaderBar); ok {
-		app.headerBar = h
-	}
-
-	obj, err = builder.GetObject("infobar1")
-	if err != nil {
-		panic(err)
-	}
-	if i, ok := obj.(*gtk.InfoBar); ok {
-		app.infoBar = i
-	}
 	app.infoBar.Connect("response", app.infoBar.Hide)
 
-	obj, err = builder.GetObject("actionbar1")
-	if err != nil {
-		panic(err)
-	}
-	if i, ok := obj.(*gtk.ActionBar); ok {
-		app.actionBar = i
-	}
-
-	obj, err = builder.GetObject("menu1")
-	if err != nil {
-		panic(err)
-	}
-	if i, ok := obj.(*gtk.Menu); ok {
-		app.menu = i
-	}
-
 	//Switch application theme
-	obj, err = builder.GetObject("isDarkTheme")
-	if err != nil {
-		panic(err)
-	}
-	if i, ok := obj.(*gtk.CheckMenuItem); ok {
-		i.Connect("toggled", func() {
-			isDark, err := app.settings.GetProperty("gtk-application-prefer-dark-theme")
-			if b, ok := isDark.(bool); ok {
-				err = app.settings.Set("gtk-application-prefer-dark-theme", !b)
-			}
-			if err != nil {
-				panic(err)
-			}
-		})
-	}
+	app.isDarkTheme.Connect("toggled", func() {
+		isDark, err := app.settings.GetProperty("gtk-application-prefer-dark-theme")
+		if b, ok := isDark.(bool); ok {
+			err = app.settings.Set("gtk-application-prefer-dark-theme", !b)
+		}
+		if err != nil {
+			panic(err)
+		}
+	})
 
 	//Enable action buttons
-	obj, err = builder.GetObject("isActionsEnabled")
-	if err != nil {
-		panic(err)
-	}
-	if i, ok := obj.(*gtk.CheckMenuItem); ok {
-		i.Connect("toggled", func() {
-			widget, err := app.actionBar.GetChild()
-			if err != nil {
-				panic(err)
-			}
-			revealerChild, err := gtk.Bin(widget).GetChild()
-			if err != nil {
-				panic(err)
-			}
+	app.isActionsEnabled.Connect("toggled", func() {
+		//widget, err := app.actionBar.GetChild()
+		//if err != nil {
+		//	panic(err)
+		//}
+		//revealerChild, err := gtk.Bin(widget).GetChild()
+		//if err != nil {
+		//	panic(err)
+		//}
 
-			name, err := revealerChild.GetName()
-			if err != nil {
-				panic(err)
-			}
+		//name, err := revealerChild.GetName()
+		//if err != nil {
+		//	panic(err)
+		//}
 
-			fmt.Println(name)
-		})
-	}
+		//fmt.Println(name)
+	})
 
 	go func() {
 		for {
@@ -123,7 +70,77 @@ func main() {
 		}
 	}()
 	app.window.Show()
+	defer gtk.MainQuit()
 	gtk.Main()
+}
+
+func GetObjects(builderFile string) (app, error) {
+	builder, err := gtk.BuilderNew()
+	if err != nil {
+		return app{}, err
+	}
+	builder.AddFromFile(builderFile)
+	var app app
+
+	settings, err := gtk.SettingsGetDefault()
+	if err != nil {
+		return app, err
+	}
+	app.settings = settings
+
+	obj, err := builder.GetObject("window1")
+	if err != nil {
+		return app, err
+	}
+	if w, ok := obj.(*gtk.Window); ok {
+		app.window = w
+	}
+
+	obj, err = builder.GetObject("headerbar1")
+	if err != nil {
+		return app, err
+	}
+	if h, ok := obj.(*gtk.HeaderBar); ok {
+		app.headerBar = h
+	}
+	obj, err = builder.GetObject("infobar1")
+	if err != nil {
+		return app, err
+	}
+	if i, ok := obj.(*gtk.InfoBar); ok {
+		app.infoBar = i
+	}
+	obj, err = builder.GetObject("actionbar1")
+	if err != nil {
+		return app, err
+	}
+	if i, ok := obj.(*gtk.ActionBar); ok {
+		app.actionBar = i
+	}
+
+	obj, err = builder.GetObject("menu1")
+	if err != nil {
+		return app, err
+	}
+	if i, ok := obj.(*gtk.Menu); ok {
+		app.menu = i
+	}
+
+	obj, err = builder.GetObject("isDarkTheme")
+	if err != nil {
+		return app, err
+	}
+	if i, ok := obj.(*gtk.CheckMenuItem); ok {
+		app.isDarkTheme = i
+	}
+	obj, err = builder.GetObject("isActionsEnabled")
+	if err != nil {
+		return app, err
+	}
+	if i, ok := obj.(*gtk.CheckMenuItem); ok {
+		app.isActionsEnabled = i
+	}
+	return app, nil
 }
 
 type hideShower interface {
